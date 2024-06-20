c870b4a67f4f5f3fb9a797eaa9196cdbc7ab8f83
aliendillcode <rendimentocontinuo@gmail.com>
2024-06-20 15:05:11 -0400

    first commit

---
+++ src/cursojava/classes/Aluno.java
@@ -0,0 +1,11 @@
+package cursojava.classes;
+/*esta é nossa classe/objeto que representa o Aluno*/
+public class Aluno {
+    String nome;
+    int idade;
+    String dataNascimento;
+    String registroGeral;
+    String numeroCpf;
+
+
+}
